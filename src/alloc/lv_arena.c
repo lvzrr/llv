fdedaa6c52cf6a9a295b8a5c4e38449d203a6c1e
lvzrr <lvzrr@proton.me>
2025-06-04 22:16:20 +0200

    passed test + arena alloc

--- src/alloc/lv_arena.c
+++ src/alloc/lv_arena.c
@@ -41,11 +41,11 @@ static t_arena	*lv_init_arena(size_t size)
 
 	t_arena *arena = lv_alloc(sizeof(t_arena));
 	if (!arena)
-		return NULL;
+		return (NULL);
 	total_size = LV_MAX(size * 2, DEFAULT_ARENA_SIZE);
 	arena->pool = lv_alloc(total_size);
 	if (!arena->pool)
-		return NULL;
+		return (NULL);
 	arena->size = total_size;
 	arena->offset = 0;
 	arena->next = NULL;
@@ -119,7 +119,7 @@ void	*lv_arena(size_t size)
 	static t_arena	*current;
 
 	if (!size)
-		return NULL;
+		return (NULL);
 	if (!arena)
 	{
 		arena = lv_init_arena(size);
